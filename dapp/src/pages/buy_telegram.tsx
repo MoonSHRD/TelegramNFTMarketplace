c8561a57927ee6ad22718cbdbb355f42e3d6b2ad
daseinsucks <alexstepov97@gmail.com>
2023-01-09 18:35:54 +0400

    makebuyoffer implemented

--- dapp/src/pages/buy_telegram.tsx
+++ dapp/src/pages/buy_telegram.tsx
@@ -120,6 +120,14 @@ const Home: NextPage = () => {
             currentAccount={currentAccount}
           />
         </Box> 
+        <Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
+          <Heading my={4}  fontSize='xl'>Buy now!</Heading>
+          <MakeBuyOffer 
+            addressContract='0xEbE648689E98abA446e38621E5a3491db03a7621'
+            marketAddress='0x2b1e0A2b16AB524Ad3f4273d1ecB63FC3b9cB58C'
+            currentAccount={currentAccount}
+          />
+        </Box> 
         
   
 
